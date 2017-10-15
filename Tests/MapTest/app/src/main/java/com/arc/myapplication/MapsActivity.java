fe2668a544e821ddc1fac322caaaa3888e91b86b
skyhawk33 <ryleyj@shaw.ca>
2017-10-14 17:23:32 -0700

    Latitude/Longitude Display
    The app now shows latitude and longitude at the bottom of the screen

--- Tests/MapTest/app/src/main/java/com/arc/myapplication/MapsActivity.java
+++ Tests/MapTest/app/src/main/java/com/arc/myapplication/MapsActivity.java
@@ -1,4 +1,4 @@
-package a300.hiking.test.trackgps;
+package com.arc.myapplication;
 
 import android.Manifest;
 import android.content.Context;
@@ -7,13 +7,22 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import java.text.DecimalFormat;
 
-public class MainActivity extends AppCompatActivity implements LocationListener {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, LocationListener{
+
+    private GoogleMap mMap;
 
     private LocationManager locationManager;
     private double lat;
@@ -24,7 +33,11 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
@@ -44,16 +57,33 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
         latView.setText((new DecimalFormat(".#####").format(locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLatitude())));
         longView.setText((new DecimalFormat(".#####").format(locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER).getLongitude())));
+
     }
 
-    //This is a comment ^^
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
     @Override
     public void onLocationChanged(Location location) {
-        //lat = location.getLatitude();
-        //longt = location.getLongitude();
         latView.setText((new DecimalFormat(".#####").format(location.getLatitude())));
         longView.setText((new DecimalFormat(".#####").format(location.getLongitude())));
-
     }
 
     @Override
