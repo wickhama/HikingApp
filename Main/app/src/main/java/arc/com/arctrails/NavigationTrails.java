035cf5a08d510e855f4f1b422c15ff549e350980
Ayla Wickham <Awickham159@hotmail.com>
2017-11-18 13:53:55 -0800

    didnt change anything

--- Main/app/src/main/java/arc/com/arctrails/NavigationTrails.java
+++ Main/app/src/main/java/arc/com/arctrails/NavigationTrails.java
@@ -1,12 +1,17 @@
 package arc.com.arctrails;
 
+import org.alternativevision.gpx.GPXParser;
+
+import java.io.FileInputStream;
+
 /**
  * Created by SamTheTurdBurgler on 2017-11-18.
  */
 
 public class NavigationTrails {
 
-    public void addTrailButtons() {
-        
+    public void parseFile() {
+        GPXParser gpxParser = new GPXParser();
+        //FileInputStream in = new FileInputStream("test.gpx");
     }
 }
