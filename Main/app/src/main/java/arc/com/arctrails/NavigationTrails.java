3f1336c7b8b6f8e149cfa0a2bb04289151421b93
Caleigh LePage <32602789+lepagec@users.noreply.github.com>
2017-11-18 12:30:48 -0800

    Revert "Added GPX Parser aw"
    This reverts commit f14a48b4baa0563b9ec2a3a9721fdb23ac27a853.

--- Main/app/src/main/java/arc/com/arctrails/NavigationTrails.java
+++ Main/app/src/main/java/arc/com/arctrails/NavigationTrails.java
@@ -4,13 +4,9 @@ package arc.com.arctrails;
  * Created by SamTheTurdBurgler on 2017-11-18.
  */
 
-public class GPXFiles {
+public class NavigationTrails {
 
-    public void readFile() {
+    public void addTrailButtons() {
         
-    }
-
-    public void writeFile() {
-
     }
 }
